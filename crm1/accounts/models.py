238897ee8b814e397c057e0308e54454113829c8
Nicolas Lopez <nicolas.lopezgonzalez@viacomcontractor.com>
2020-10-09 08:48:00 -0300

    Implemented models and products template

--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -12,24 +12,38 @@ class Customer(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Outdoor', 'Outdoor'),
+    )
+
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
-    category = models.CharField(max_length=200, null=True)
-    description = models.CharField(max_length=200, null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
     STATUS = (
-        ('Pending','Pending'),
-        ('Out for delivery','Out for delivery'),
-        ('Delivered', 'Delivered')
-
-
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered'),
     )
 
-    # customer =
-    # product =
+    customer = models.ForeignKey(Customer,null=True,on_delete= models.SET_NULL)
+    product = models.ForeignKey(Product,null=True,on_delete= models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=200, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Product
 
 # Create your views here.
+
 
 def home(request):
     return render(request, 'accounts/dashboard.html')
 
 
 def products(request):
-    return render(request, 'accounts/products.html')
+
+    product_list = Product.objects.all()
+    return render(request, 'accounts/products.html',{'products' : product_list})
 
 
 def customer(request):
