b677d3f920a052b72daa715199a0aa95bc71b5dd
Nicolas Lopez <nicolas.lopezgonzalez@viacomcontractor.com>
2020-10-12 14:48:48 -0300

    Implemented CRUD for order

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -1,19 +1,75 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Product
+from .models import *
+from .forms import OrderForm
+
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders_list = Order.objects.all()
+    customers_list = Customer.objects.all()
+    total_customers = customers_list.count()
+    total_orders = orders_list.count()
+    delivered = orders_list.filter(status='Delivered').count()
+    pending = orders_list.filter(status='Pending').count()
+
+    context = {'orders': orders_list, 'customers': customers_list,
+               'total_customers': total_customers, 'delivered': delivered,
+               'total_orders': total_orders, 'pending': pending}
+
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def products(request):
-
     product_list = Product.objects.all()
-    return render(request, 'accounts/products.html',{'products' : product_list})
+    return render(request, 'accounts/products.html', {'products': product_list})
 
 
-def customer(request):
-    return render(request, 'accounts/curstomer.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    order_count = orders.count()
+
+    context = {'customer': customer, 'orders': orders, 'order_count': order_count}
+
+    return render(request, 'accounts/customer.html', context)
+
+
+def createOrder(request):
+    form = OrderForm()
+    if request.method == 'POST':
+        # print('Printing post: ', request.POST)
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'accounts/order_form.html', context)
+
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+    if request.method == 'POST':
+        # print('Printing post: ', request.POST)
+        form = OrderForm(request.POST,instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'accounts/order_form.html', context)
+
+
+def deleteOrder(request,pk):
+    order = Order.objects.get(id=pk)
+
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+
+    context = {'item':order}
+    return render(request, 'accounts/delete.html',context)
--- crm1/crm1/settings.py
+++ crm1/crm1/settings.py
@@ -64,6 +64,8 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins': ['django.templatetags.static']
+
         },
     },
 ]
